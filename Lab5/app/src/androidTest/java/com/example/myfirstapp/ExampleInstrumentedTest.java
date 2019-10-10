35c2770b6b6f7b3aa583fdadafcd1f3494fe82ab
RuAlmalki <50450137+RuAlmalki@users.noreply.github.com>
2019-10-10 13:34:13 -0400

    new

--- Lab5/app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
+++ Lab5/app/src/androidTest/java/com/example/myfirstapp/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
-package com.example.lab5;
+package com.example.myfirstapp;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -21,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.lab5", appContext.getPackageName());
+        assertEquals("com.example.myfirstapp", appContext.getPackageName());
     }
 }
