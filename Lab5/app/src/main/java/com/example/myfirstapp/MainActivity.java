8877c362e917fd70dca765cf896cade208776644
RuAlmalki <50450137+RuAlmalki@users.noreply.github.com>
2019-10-11 07:33:04 -0400

    part3

--- Lab5/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ Lab5/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
